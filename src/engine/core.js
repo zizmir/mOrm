144cdfd634a74930ef62b64d3257f74a55168483
De la fuente <delafuentecindy@gmail.com>
2019-01-25 15:59:23 +0100

    mdump

--- src/engine/core.js
+++ src/engine/core.js
@@ -1,8 +1,10 @@
+import { mDump } from "../libs/mDump";
+
 export default class Core {
   constructor({ host, port, username, password, database }) {
     this.obj = { host, port, username, password, database };
   }
-  dump() {
+  dump(is_uri = false) {
     console.log(this.obj);
   }
 }
--- src/entities/entity.js
+++ src/entities/entity.js
@@ -1,5 +1,8 @@
+import pg from "pg";
+
 export default class Entity {
   constructor(dbInstance, name) {
     (this.dbInstance = dbInstance), (this.name = name);
   }
+  async save(data) {}
 }
