b999ca723b62eb5f9a5d7b937fa5836837d111cc
m'bark <mbark.bouznir@gmail.com>
2019-01-25 11:24:19 +0100

    parse uri

--- src/mOrm.js
+++ src/mOrm.js
@@ -1,19 +1,33 @@
-// import { isEmpty } from "lodash";
-// export default class mOrm {
-//   async createConnection(dbConfig = {}){
-//     if ( typeof cdbcConfig == 'string'){
-//
-//     }
-//
-//
-//   }
-//
-//   const { host, port , username , pass } = this.config
-//   switch (type) {
-//     case 'postgres':
-//         this dbInstance = new PostgresSQL({ host, port , username , pass })
-//       break;
-//     default:
-//
-//   }
-// }
+import { isEmpty } from "lodash";
+export default class mOrm {
+  constructor() {
+    this.config = {};
+  }
+  configPathName = "./mOrm.config.js";
+  // { host, port , username , pass }
+  // postgres
+  async createConnection(dbConfig = {}) {
+    // regex
+    // check
+    if (isEmpty(dbConfig)) {
+    }
+    if (typeof dbConfig === "string") {
+      // postgres://user:pass@host:port/db
+      // regx
+      let [, type, username, password, host, port, database] = dbConfig.match(
+        /(.*):\/\/(.*):(.*)@(.*):(.*)\/(.*)/
+      );
+      this.config = { type, host, port, username, password, database };
+      console.log(this.config);
+    }
+  }
+  //
+  // const { host, port , username , pass } = this.config
+  // switch (type) {
+  //   case 'postgres':
+  //       this.dbInstance = new PostgresSQL({ host, port , username , pass })
+  //     break;
+  //   default:
+  //
+  // }
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
-console.log("hello");
+import mOrm from "./mOrm";
+
+let db = new mOrm();
+
+let result = db.createConnection("postgres://user:pass@host:port/db");
