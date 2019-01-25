43bdb51870be9a21f739301943ca42482d814abc
m'bark <mbark.bouznir@gmail.com>
2019-01-25 10:29:56 +0100

    base

---
+++ mOrm.js
@@ -0,0 +1,19 @@
+// import { isEmpty } from "lodash";
+// export default class mOrm {
+//   async createConnection(dbConfig = {}){
+//     if ( typeof cdbcConfig == 'string'){
+//
+//     }
+//
+//
+//   }
+//
+//   const { host, port , username , pass } = this.config
+//   switch (type) {
+//     case 'postgres':
+//         this dbInstance = new PostgresSQL({ host, port , username , pass })
+//       break;
+//     default:
+//
+//   }
+// }
